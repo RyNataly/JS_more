3c5ac102c57645f07b3d78e005ba23df56217ca9
Natalia Ryzhenko <ryzhena@gmail.com>
2024-09-25 22:26:13 +0300

    4 - функции, строки

--- script_hard.js
+++ script_hard.js
@@ -1,35 +1,18 @@
- 'use strict';
- 
-let lang = prompt("Введите значение lang (en, ru)", "ru");
+'use strict';
 
-if (lang == "en") {
-   console.log("monday, tuesday, wednesday, thursday, friday, saturday, sunday");
- } else if (lang == "ru"){
-   console.log("понедельник, вторник, среда, четверг, пятница, суббота, воскресенье");
- } else
-    console.log("Что-то пошло не так!");
+let intMy = 12;
 
- switch (true){
-  case lang == "en":
-    console.log("monday, tuesday, wednesday, thursday, friday, saturday, sunday");
-    break;
-  case lang == "ru":
-    console.log("понедельник, вторник, среда, четверг, пятница, суббота, воскресенье");
-    break;
-  default: 
-    console.log("Что-то пошло не так!");
- }
+const myString = function (title) { 
+  if (typeof title !== "string"){
+      console.log("Это не строка!");
+  } else {
+    console.log(title = title.trim());
+    if (title.length > 30) {
+      // сonsole.log(title.length);
+      console.log(title.slice(0, 30) + "...");
+    }
+  }
+}
 
- //C массивом не заладилось. Есть идея как сделать через ассоциативный массив, а через многомерный - не хватает знаний по выводу, подозреваю, что есть хитрый вывод. Я пока не нашла. 
- let studentsData = [['monday', 'понедельник'], ['tuesday', 'вторник']];
-
-// перебираем элементы массива studentsData
-studentsData.forEach((day) => {
-        console.log(day[0]);
-
-});
-
-// У нас есть переменная namePerson. Если значение этой переменной “Артем” то вывести в консоль “директор”, если значение “Александр” то вывести в консоль “преподаватель”, с любым другим значением вывести в консоль “студент”
-let namePerson = prompt("Введите имя");
-namePerson == "Артем" ? console.log("директор") : namePerson == "Александр" ? console.log("преподаватель") : console.log("студент");
-
+myString(intMy);
+myString(prompt("Введите строку: "));
