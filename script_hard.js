033b83d601967c77840d5fbe5b32f74f44022c1a
Natalia Ryzhenko <ryzhena@gmail.com>
2024-09-29 22:11:02 +0300

    7 - циклы, массив, дни недели форматированные

--- script_hard.js
+++ script_hard.js
@@ -1,5 +1,18 @@
  'use strict';
- 
- let lang = prompt("Первый коммит в dev");
+// Загадывание случайного числа от 1 до 100
+let week = ['пн', 'вт', 'ср', 'чт', 'пт', "сб", 'вс']
 
+const date = new Date();
+// console.log(week[ date.getDay() > 0 ? date.getDay()-1 : 6]) 
+
+for (let key in week)  {
+    if (key > 4 && key != (date.getDay() > 0 ? date.getDay()-1 : 6)) 
+      console.log("%c" + week[key], "font-style: italic;");
+    else if (key > 4 && key == (date.getDay() > 0 ? date.getDay()-1 : 6)) 
+      console.log("%c" + week[key], "font-weight: bold;");
+    else if (key === (date.getDay() > 0 ? date.getDay()-1 : 6))
+      console.log("%c" + week[key], "font-weight: bold;"); 
+    else 
+      console.log(week[key]);
+};
 
